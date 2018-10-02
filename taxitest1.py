3c9e79171202b9470a2ea5ead6b369b11f098bad
jc490690 <morgan.wallish@my.jcu.edu.au>
2018-10-02 16:02:46 +1000

    #done #Test #move

---
+++ taxitest1.py
@@ -0,0 +1,8 @@
+from taxi import Taxi
+
+new_taxi = Taxi("Prius 1", 100)
+new_taxi.drive(40)
+print(new_taxi)
+new_taxi.get_fare()
+new_taxi.drive(100)
+print(new_taxi)
---
+++ unreliable test2.py
@@ -0,0 +1,4 @@
+from unreliablecar import Unreliable
+
+unreliablecar = Unreliable("bomb", 100, 60)
+unreliablecar.drive(120)
